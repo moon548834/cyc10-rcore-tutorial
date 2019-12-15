f18127db96dd99f7d6705c9e6f7ded596d189998
moon548834 <fzp548834@163.com>
2019-12-15 10:29:41 +0800

    update doc

--- update_book.sh
+++ update_book.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-rm -rf docs
-mv _book docs
-#cd docs
-#rm update_book.sh
-#git add .
-#git commit -m "update web"
-#cd ..
+#rm -rf docs
+#mv _book docs
+cd docs
+rm update_book.sh
+git add .
+git commit -m "update web"
+cd ..
 #git add .
 #git commit -m "update doc"
 #git push origin master
