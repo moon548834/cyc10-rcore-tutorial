01fc1a45cd95bacd3aac1fd0d055e348424c1c6e
moon548834 <fzp548834@163.com>
2019-12-16 17:49:19 +0800

    [M] update web

---
+++ docs/update_book.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+rm -rf docs && gitbook build
+#mv _book docs
+#cd docs
+#rm update_book.sh
+#git add .
+#git commit -m "update web"
+#cd ..
+#git add .
+#git commit -m "update doc"
+#git push origin master
